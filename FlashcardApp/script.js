47719023d5aa152962c7af5a2c5c89f73e04b48f
nchaki1 <nchakis@outlook.com>
2024-04-21 21:07:29 -0400

    base

--- FlashcardApp/script.js
+++ FlashcardApp/script.js
@@ -3,6 +3,20 @@ const answer = document.querySelector(".answer")
 const showButton = document.querySelector(".show");
 const nextButton = document.querySelector(".next");
 
+let questions = {
+    "dog":"dogcat",
+    "cat":"catdog:"
+}
+
+let terms = Object.entries(questions);
+
+function selectTerm() {
+    let randTerm = terms[Math.floor(Math.random() * terms.length)][0];
+    console.log(randTerm);
+}
+
+selectTerm();
+
 showButton.addEventListener('click', function() {
     answer.style.display = "block";
 });
