d2478a783f250ef22ecc1e996a5a032511d08509
nchaki1 <nchakis@outlook.com>
2024-04-21 20:36:13 -0400

    base

---
+++ FlashcardApp/script.js
@@ -0,0 +1,11 @@
+const term = document.querySelector(".term");
+const flipButton = document.querySelector(".flip");
+const nextButton = document.querySelector(".next");
+
+flipButton.addEventListener('click', function() {
+    console.log("You clicked flip button");
+})
+
+nextButton.addEventListener('click', function() {
+    console.log("You clicked next button");
+})
