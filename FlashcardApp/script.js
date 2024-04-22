094f8e73d08525656d3cc3b089a9f71f3f689518
nchaki1 <nchakis@outlook.com>
2024-04-21 20:49:05 -0400

    base

--- FlashcardApp/script.js
+++ FlashcardApp/script.js
@@ -5,8 +5,8 @@ const nextButton = document.querySelector(".next");
 
 showButton.addEventListener('click', function() {
     answer.style.display = "block";
-})
+});
 
 nextButton.addEventListener('click', function() {
     console.log("You clicked next button");
-})
+});
