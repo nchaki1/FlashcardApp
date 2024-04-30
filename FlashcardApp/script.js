4a8a2a48b7688a26b5c6f4b0622183fb3f9465d0
nchaki1 <nchakis@outlook.com>
2024-04-29 21:19:30 -0400

    database

--- FlashcardApp/script.js
+++ FlashcardApp/script.js
@@ -1,20 +1,28 @@
 const cards = document.querySelector(".cards")
 const category = document.querySelector(".category")
 
-let questions = {
-    "Dog":"Dogs are barky",
-    "Cat":"Cats are furry",
-    "Snail":"Snails are slow",
-    "Bee":"Bees are busy"
-}
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./flash.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected');
+  });
 
 let categories = ["Animals", "Not Animals"]
 
 category.innerHTML = `<h2>${categories[0]}</h2>`
 
-let terms = Object.entries(questions)
-
 function displayTerms() {
+    let questions = {
+        "Dog":"Dogs are barky",
+        "Cat":"Cats are furry",
+        "Snail":"Snails are slow",
+        "Bee":"Bees are busy"
+    }
+
+    let terms = Object.entries(questions)
+
     let x = 0
     for (term in terms) {
         cards.insertAdjacentHTML('beforeend', `<div class="cards"><h3>${terms[x][0]}</h3></div>`);
